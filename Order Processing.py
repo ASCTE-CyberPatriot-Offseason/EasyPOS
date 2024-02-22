a53e039f6aaae7605da9526cc56e2cd0188fbcc8
ChristopherEsker <christopher.esker@ascte.org>
2024-02-22 16:31:40 -0600

    e

--- Order Processing.py
+++ Order Processing.py
@@ -4,36 +4,15 @@
 # Ability to modify or remove items from an order before finalizing. 
 # Order status tracking (e.g., pending, in-progress, completed).
 
-menu = ["Burger", "Fries", "Drink"]
+menu = ["Burger", "Taco", "Fries", "Drink", "Cookie"]
 order = []
 
-
-def orderer():
-    order1 = input("What would you like to order? ")
-    if order1 == "burger":
-        burger()
-    if order1 == "fries":
-        fries()
-
-def burger():
-    order.append("Burger")
-    friesYorN = input("Would you like to make it a combo? ")
-    if friesYorN == "yes":
-        fries()
-
-def fries():
-    frySize = input("What size do you want your fries to be? ")
-    if frySize == "small":
-        order.append("Small Fries")
-    if frySize == "medium":
-        order.append("Medium Fries")
-    if frySize == "large":
-        order.append("Large Fries")
 
 def addItem():
     orderAgain = True
     while orderAgain == True:
-        orderer()
+        orderNum = int(input("What would you like to order? (enter number) "))
+        order.append(menu[orderNum - 1])
         print(f"This is your current order: \n{order}")
         orderAgainYN = input("Would you like to order another item? ")
         if orderAgainYN != "yes":
@@ -43,11 +22,10 @@ def removeItem():
     removeItem = True
     while removeItem == True:
         print(f"This is your current order: \n{order}")
-        removeitemYN = input("Would you like to remove an item? ")
-        if removeitemYN == "yes":
-            remove = int(input("What is the index of it? ")) # Make better
-            order.pop(remove)
-        else:
+        remove = int(input("What is the index of the item you want to remove? ")) # Make input system better
+        order.pop(remove)
+        removeAgain = input("Would you like to remove something else? ")
+        if removeAgain != "yes":
             removeItem = False
 
 print()
@@ -57,6 +35,14 @@ for i in range(len(menu)):
 
 print()
 addItem()
-removeItem()
-
+while True:
+    anythingElse = input("Can I help you with anything else? ")
+    if anythingElse == "yes":
+        addOrRemove = input("Do you want to add or remove an item? ")
+        if addOrRemove == "add":
+            addItem()
+        if addOrRemove == "remove":
+            removeItem()
+    else:
+        break
 print("GOOODBYE")
