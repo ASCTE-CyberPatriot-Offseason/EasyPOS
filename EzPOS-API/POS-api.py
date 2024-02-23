e55d82c8eb52f364df4c3d92673850536b30aa78
Justin Wright <justwright916@gmail.com>
2024-02-23 02:47:00 -0600

    Second step for man
    (cherry picked from commit b26aad518e54b2ebbb90570e7e99b82e9edacc2d)

--- EzPOS-API/POS-api.py
+++ EzPOS-API/POS-api.py
@@ -10,6 +10,40 @@ api = Api(app)
 rms ={
     'restaurant' : {'name' : 'KFC', 'Phone Number' : '123-456-7890','address' : 'god help me lane' },
     'menu' : {
-        'wings' : {'price' : 10.99, 'quantity' : 10},
+        'wings' : {'price' : '10.99', 'quantity_remaining' : '10'},
+        'fries' : {'price' : '3.99', 'quantity_remaining' : '20'},
+        'drink' : {'price' : '1.99', 'quantity_remaining' : '50'}
     }
 }
+
+class restaurant(Resource):
+    def get(self):
+        return rms['restaurant']
+    
+class menu(Resource):
+    def get(self, item):
+        try:
+            return rms['menu'][item]
+        except KeyError:
+            return{}
+    def put(self, item):
+        try:
+            rms['menu'][item]
+        except KeyError:
+            return {f'Error{item} is not on the menu'}
+        
+        parser = reqparse.RequestParser()
+        parser.add_argument('price')
+        parser.add_argument('quantity_remaining')
+        result = parser.parse_args()
+        print(result)
+
+#Mapping classes to different end points
+api.add_resource(restaurant, '/restaurant')
+api.add_resource(menu, '/menu/<item>')
+
+#Starting the server
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
